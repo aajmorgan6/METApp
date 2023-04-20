6376cdc160baf667f12bb1a71c5e39249317ca14
aajmorgan <ajmsd6@gmail.com>
2023-04-19 21:59:01 -0400

    Initial Commit

---
+++ Art/ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtApp.swift
+//  Art
+//
+//  Created by AJ Morgan on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
